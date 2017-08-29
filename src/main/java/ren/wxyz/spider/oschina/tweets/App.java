f2897e45145649a71678fd79dac7a8a25d1f8c90
wxyz <hyhjwzx@126.com>
2017-08-29 19:43:32 +0800

    添加开始和关闭日志

--- src/main/java/ren/wxyz/spider/oschina/tweets/App.java
+++ src/main/java/ren/wxyz/spider/oschina/tweets/App.java
@@ -1,11 +1,19 @@
 package ren.wxyz.spider.oschina.tweets;
 
+import lombok.extern.slf4j.Slf4j;
+
 /**
  * 启动类
  *
  * @author wxyz 2017-08-29_19:28
  */
+@Slf4j
 public class App {
+
+    static {
+        log.info("start app");
+        shutdownHook();
+    }
 
     /**
      * 启动方法
@@ -16,5 +24,16 @@ public class App {
      */
     public static void main(String[] args) {
         System.out.println("Hello World!");
+    }
+
+    /**
+     * 应用关闭事件处理
+     */
+    private static void shutdownHook() {
+        Runtime.getRuntime().addShutdownHook(new Thread() {
+            public void run() {
+                log.info("stop app");
+            }
+        });
     }
 }
